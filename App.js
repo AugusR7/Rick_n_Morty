a5e76f886b89b8d0600e4ed495597cec81429b7d
AugusR7 <augusramseyer197@gmail.com>
2022-09-10 19:25:57 -0300

    Creación repo

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image } from 'react-native';
 
 export default function App() {
+  const [characters, setCharacters] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const obtenerPersonajes = async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character/1,2');
+    const json = await response.json();
+    setCharacters(json)
+  }
+
+  obtenerPersonajes();
+  // console.log(characters);
+
+  // fetch('https://rickandmortyapi.com/api/character/1,2')
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       var results = response.results
+  //       setCharacters(results);
+  //       console.log(results);
+  //       // setCharacters(response.results);
+  //       // setCharacters(response);
+  //       console.log(response);
+  //       // console.log("--------------------------");
+  //       // console.log(response.name);
+  //     });
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Prueba piloto!</Text>
+      {/* <FlatList>
+          {characters.map((item, index) => (
+            <Text key={index}>
+              {item.name}
+            </Text>
+          ))}
+        </FlatList> */}
     </View>
   );
 }
