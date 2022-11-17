cc4cf150adfec19319fe8fea26acab4296350fab
aguskrembs <72949652+aguskrembs@users.noreply.github.com>
2022-11-16 22:59:47 -0300

    random fixes

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -20,11 +20,18 @@ import FilterScreenRenderer from "./components/Filter/FilterScreenRenderer";
 import styles from "./appStyles";
 // import { Header } from "react-native/Libraries/NewAppScreen";
 // Redux imports
-import {useSelector, useDispatch} from 'react-redux';
-import { fetchInitialCharacters, charactersSelector, fetchNewCharacters, fetchFilteredCharacters, writeFavouriteCharacter, removeFavouriteCharacter, addFavouriteCharacter, addNewFavouriteCharacter, removeAFavouriteCharacter } from "./slices/characters";
-
-
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchInitialCharacters,
+  charactersSelector,
+  fetchNewCharacters,
+  fetchFilteredCharacters,
+  writeFavouriteCharacter,
+  removeFavouriteCharacter,
+  addFavouriteCharacter,
+  addNewFavouriteCharacter,
+  removeAFavouriteCharacter,
+} from "./slices/characters";
 
 const AVATAR_SIZE = 150;
 const SPACING = 20;
@@ -33,20 +40,24 @@ const HEIGHT = PixelRatio.getPixelSizeForLayoutSize(60);
 const Drawer = createDrawerNavigator();
 
 export default function mainScreen() {
-    // ---------------------------------- Redux declarations ---------------------------------- //
+  // ---------------------------------- Redux declarations ---------------------------------- //
   const dispatch = useDispatch();
-  const { characters, favouriteCharacters, favouriteCharactersId, loading, hasErrors } = useSelector(charactersSelector);
+  const {
+    characters,
+    favouriteCharacters,
+    favouriteCharactersId,
+    loading,
+    hasErrors,
+  } = useSelector(charactersSelector);
 
   useEffect(() => {
     dispatch(fetchInitialCharacters());
   }, [dispatch]);
 
-
   // ---------------------------------- State declarations ---------------------------------- //
   const [showModal, setShowModal] = useState(false);
   const [characterModal, setCharacterModal] = useState({});
   const scrollY = React.useRef(new Animated.Value(0)).current;
-
 
   const getNewCharactersFromAPI = () => {
     dispatch(fetchNewCharacters());
@@ -69,8 +80,8 @@ export default function mainScreen() {
     setCharacterModal({});
   };
 
-  const toggleFavouriteHandler = (character) =>{
-    if(favouriteCharactersId.includes(character.id)){
+  const toggleFavouriteHandler = (character) => {
+    if (favouriteCharactersId.includes(character.id)) {
       dispatch(removeAFavouriteCharacter(character));
       // console.log("Borró..."+character.name);
     } else {
@@ -79,11 +90,10 @@ export default function mainScreen() {
     }
   };
 
-
   // ---------------------------------- Character Render ---------------------------------- //
   function characterRender({ item, index }) {
     const inputRange = [-1, 0, ITEM_SIZE * index, ITEM_SIZE * (index + 2)];
-    // const [characterFavorite, setCharacterFavorite] = useState(false); 
+    // const [characterFavorite, setCharacterFavorite] = useState(false);
     // Esto no anda porque no deja que se use un "useState" en una funcion y no se que onda
     const opacityInputRange = [
       -1,
@@ -91,12 +101,12 @@ export default function mainScreen() {
       ITEM_SIZE * index,
       ITEM_SIZE * (index + 1),
     ];
-    
+
     const scale = scrollY.interpolate({
       inputRange,
       outputRange: [1, 1, 1, 0],
     });
-    
+
     const opacity = scrollY.interpolate({
       inputRange: opacityInputRange,
       outputRange: [1, 1, 1, 0],
@@ -125,17 +135,27 @@ export default function mainScreen() {
           <Image style={styles.image} source={{ uri: item.image }} />
           <View style={styles.starContainer}>
             <TouchableOpacity
-            onPress={() => {toggleFavouriteHandler(item)}}>
-            { favouriteCharactersId.includes(item.id)? (
-              <Image style={styles.star} source={require("./components/yellow-star.png")} /> 
-              ):(
-              <Image style={styles.star} source={require("./components/white-star.png")} />
+              onPress={() => {
+                toggleFavouriteHandler(item);
+              }}
+            >
+              {favouriteCharactersId.includes(item.id) ? (
+                <Image
+                  style={styles.star}
+                  source={require("./components/yellow-star.png")}
+                />
+              ) : (
+                <Image
+                  style={styles.star}
+                  source={require("./components/white-star.png")}
+                />
               )}
-
             </TouchableOpacity>
           </View>
           <View style={styles.nameContainer}>
             <Text style={styles.text}>{item.name}</Text>
+            <Text style={styles.text}>{item.species}</Text>
+            <Text style={styles.text}>{item.type}</Text>
           </View>
         </Animated.View>
       </TouchableOpacity>
@@ -147,7 +167,7 @@ export default function mainScreen() {
     return (
       <SafeAreaView style={styles.SAVcontainer}>
         <HeaderBar closeHandler={closeHandler} />
-      
+
         {loading ? (
           <ActivityIndicator size="large" animating={loading} />
         ) : (
@@ -182,7 +202,10 @@ export default function mainScreen() {
     return (
       <SafeAreaView style={styles.SAVcontainer}>
         <HeaderBar closeHandler={closeHandler} />
-        <FilterScreenRenderer acceptHandler={acceptHandler} closeHandler={closeHandler} />
+        <FilterScreenRenderer
+          acceptHandler={acceptHandler}
+          closeHandler={closeHandler}
+        />
       </SafeAreaView>
     );
   }
@@ -192,20 +215,20 @@ export default function mainScreen() {
       <SafeAreaView style={styles.SAVcontainer}>
         <HeaderBar closeHandler={closeHandler} />
         <Animated.FlatList
-            style={styles.flatlistStyle}
-            keyExtractor={(item) => item.id}
-            data={favouriteCharacters}
-            renderItem={characterRender}
-            contentContainerStyle={{
-              padding: SPACING,
-              paddingTop: 0,
-            }}
-            onScroll={Animated.event(
-              [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-              { useNativeDriver: true }
-            )}
-          />
-          <Modal transparent={true} visible={showModal} animationType="slide">
+          style={styles.flatlistStyle}
+          keyExtractor={(item) => item.id}
+          data={favouriteCharacters}
+          renderItem={characterRender}
+          contentContainerStyle={{
+            padding: SPACING,
+            paddingTop: 0,
+          }}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: true }
+          )}
+        />
+        <Modal transparent={true} visible={showModal} animationType="slide">
           <CharacterDetails
             character={characterModal}
             closeHandler={closeHandler}
@@ -215,41 +238,41 @@ export default function mainScreen() {
     );
   }
 
-  return(
+  return (
     <NavigationContainer>
-        <Drawer.Navigator
-          screenOptions={{
-            drawerStyle: {
-              backgroundColor: "black",
-            },
-            headerShown: false,
-            drawerActiveBackgroundColor: "lightyellow",
-            drawerLabelStyle: {
-              marginLeft: 25,
-              fontSize: 15,
-              fontWeight: "bold",
-              color: "darkgrey",
-            },
-          }}
-          useLegacyImplementation={true}
-          initialRouteName="HomeScreen"
-        >
-          <Drawer.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{ headerTitle: () => <HeaderBar name="HomeScreen" /> }}
-          />
-          <Drawer.Screen
-            name="FilterScreen"
-            component={FilterScreen}
-            options={{ headerTitle: () => <HeaderBar name="FilterScreen" /> }}
-          />       
-          <Drawer.Screen
-            name="FavoritesScreen"
-            component={FavoritesScreen}
-            options={{ headerTitle: () => <HeaderBar name="FavoritesScreen" /> }}
-          />
-        </Drawer.Navigator>
-      </NavigationContainer>
-  )
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: "black",
+          },
+          headerShown: false,
+          drawerActiveBackgroundColor: "lightyellow",
+          drawerLabelStyle: {
+            marginLeft: 25,
+            fontSize: 15,
+            fontWeight: "bold",
+            color: "darkgrey",
+          },
+        }}
+        useLegacyImplementation={true}
+        initialRouteName="HomeScreen"
+      >
+        <Drawer.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerTitle: () => <HeaderBar name="HomeScreen" /> }}
+        />
+        <Drawer.Screen
+          name="FilterScreen"
+          component={FilterScreen}
+          options={{ headerTitle: () => <HeaderBar name="FilterScreen" /> }}
+        />
+        <Drawer.Screen
+          name="FavoritesScreen"
+          component={FavoritesScreen}
+          options={{ headerTitle: () => <HeaderBar name="FavoritesScreen" /> }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
